14e3919aee7784d37688756cc2a337bd274314da
nailyk-fr <nailyk_git@nailyk.fr>
2018-04-12 19:52:27 +0200

    sony-init: extract_elf_ramdisk: Implement compression detect
     * Detect if ramdisk is gzip, lzma.
     * Fail if unsupported.
    
    Change-Id: I3fecc7b011cc433e9445fbc6b1e08c894bd60ca0
    Signed-off-by: nailyk-fr <nailyk_git@nailyk.fr>

--- extract_ramdisk/Android.mk
+++ extract_ramdisk/Android.mk
@@ -7,10 +7,12 @@ LOCAL_SRC_FILES := uncompress.cpp \
 
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_STATIC_LIBRARIES := libelf libc libm libz
+LOCAL_STATIC_LIBRARIES += liblzma
 LOCAL_C_INCLUDES := \
 	external/elfutils/libelf \
 	external/zlib
 LOCAL_CPPFLAGS := -g -c -W
+
 LOCAL_MODULE := extract_ramdisk
 LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES

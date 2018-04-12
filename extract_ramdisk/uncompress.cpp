27e9bb9fef573593c224670e2d53d93d47f1c44f
nailyk-fr <nailyk_git@nailyk.fr>
2018-04-12 19:52:27 +0200

    [WIP][DNM] nailyk's crap: sony-init: extract_elf_ramdisk: Implement compression detect
     * Detect if ramdisk is gzip, lzma.
     * Fail if unsupported.
    
    Change-Id: I3fecc7b011cc433e9445fbc6b1e08c894bd60ca0
    Signed-off-by: nailyk-fr <nailyk_git@nailyk.fr>

--- extract_ramdisk/uncompress.cpp
+++ extract_ramdisk/uncompress.cpp
@@ -13,6 +13,13 @@
 
 #include "uncompress.h"
 
+#define MAX_DEBUG_HEX_OUTPUT 16
+
+// memcmp needs buffer, no way to use preprocessor var
+char gzip_header[] = { 0x1f, 0x8B };
+//char lzma_header[] = { 0xFF, 0x4C, 0x5A, 0x4D, 0x41, 0x00 };
+char lzma_header[] = { 0x5d, 0x00, 0x00, 0x80, 0x00, 0xff };
+
 size_t uncompress_gzip_memory(const byte_p compressed_data,
     const size_t compressed_data_size, const byte_p uncompressed_data,
     const size_t uncompressed_max_size) {
@@ -46,13 +53,33 @@ size_t uncompress_memory(byte_p uncompressed_buffer, byte_p buffer, unsigned lon
         printf("Unable to malloc memory for gunzip.\nFailed\n");
         return -1; 
     }   
-    size_t uncompressed_size;
-    uncompressed_size = uncompress_gzip_memory(buffer, file_size,
-        uncompressed_buffer, MEMORY_BUFFER_SIZE);
+    size_t uncompressed_size = 0;
+
+    if (!memcmp(gzip_header, buffer, sizeof(gzip_header))) {
+        printf("GZIP ramdisk found\n");
+		uncompressed_size = uncompress_gzip_memory(buffer, file_size,
+	        uncompressed_buffer, MEMORY_BUFFER_SIZE);
+#ifdef USE_LZMA
+    } else if (!memcmp(lzma_header, buffer, sizeof(lzma_header))) {
+        printf("LZMA ramdisk found\n");
+		return -255; //Unsupported yet
+#endif
+    } else {
+		char header[MAX_DEBUG_HEX_OUTPUT];
+		memcpy(header, buffer, sizeof((char*)16));
+        printf("Unrecognized ramdisk compression (");
+		int i=0; 
+		for (i=0; i<MAX_DEBUG_HEX_OUTPUT ; i++) {
+			printf("0x%x", header[i]);
+		}
+		printf("). Giving up! \n");
+		return -2;
+    }
+
     free(buffer);
     if (uncompressed_size <= 0) {
         free(uncompressed_buffer);
-        printf("Failed to gunzip\n");
+        printf("Failed to uncompress\n");
         return -1; 
     }   
     printf("Original size: %lu, gunzipped: %zu\n", file_size,
